bd72be63e9d87689862ffb642e603ea79c859c50
Ho-Yi-Bin <chobits0202>
2015-06-08 17:37:20 +0800

    test

---
+++ log-report-core/src/test/java/org/yfr/CodeLogTest.java
@@ -0,0 +1,37 @@
+
+package org.yfr;
+
+import javax.annotation.Resource;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.yfr.repository.CodeLogDetailRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:conf/applicationContext.xml"})
+public class CodeLogTest {
+
+	@Resource
+	CodeLogDetailRepository codeLogDetailRepository;
+
+	@Before
+	public void up() {
+		System.out.println("first up !!");
+	}
+	
+	@Test
+	public void test() {
+		
+		System.out.println(codeLogDetailRepository.query(1000, 1003));
+	}
+
+	@After
+	public void clean() {
+		System.out.println("finall clear !!");
+	}
+
+}
